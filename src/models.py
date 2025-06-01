169e42b16c9e0034904eac5720fac9e702ae6926
TobiLonardi <123268672+TobiLonardi@users.noreply.github.com>
2025-06-01 18:47:22 +0000

    primer push

--- src/models.py
+++ src/models.py
@@ -1,19 +1,47 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, DateTime, func, Text, Table,Column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
+
+
+class Follower(db.Model):
+    __tablename__="follower"
+
+    id:Mapped[int] = mapped_column(primary_key=True)
+    user_to_id:Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    owner_user: Mapped["User"] = relationship(back_populates="followers")
+
+
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    __tablename__="user"
+    id:Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(50), nullable=False)
+    first_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(80), nullable=False, unique=True)
+    followers:Mapped[list["Follower"]] = relationship(back_populates="owner_user")
+    post:Mapped["Post"] = relationship(back_populates="users")
+    media:Mapped["Media"] = relationship(back_populates="owners")
+    
+
+class Media(db.Model):
+    __tablename__="media"
+    id:Mapped[int] = mapped_column(primary_key=True)
+    type:Mapped[int] = mapped_column(nullable=False)
+    url:Mapped[str] = mapped_column(Text, nullable=False)
+    post_id:Mapped[int]=mapped_column(ForeignKey("post.id"), nullable=False)
+    owner:Mapped[list["Post"]]= relationship(back_populates="media")
+
+    
+class Post(db.Model):
+    __tablename__="post"
+    id:Mapped[int] = mapped_column(primary_key=True)
+    user_id:Mapped[int]=mapped_column(ForeignKey("user.id"), nullable=False)
+    users:Mapped[list["User"]]= relationship(back_populates="post")
+    media:Mapped["Media"]= relationship(back_populates="owner")
 
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+
+    
